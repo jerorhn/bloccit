d07829cee14fabe4f85ad0dc9b133b2e6dd9b766
Hannah Jeror <jerorhn@yahoo.com>
2018-03-19 16:17:25 -0400

    Add questions

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,6 +19,18 @@ posts = Post.all
 end
 Comment.find_or_create_by!(post: posts.sample, body: "Sample Comment")
 
+
+#Create Questions
+50.times do
+  Question.create!(
+    title: RandomData.random_sentence,
+    body: RandomData.random_paragraph,
+    resolved: rand(2) == 1
+  )
+end
+questions = Question.all
+
 puts "Seed finished"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
+puts "#{Question.count} questions created"
