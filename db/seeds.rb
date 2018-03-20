1ae00bd5d223fbb0fa3ef38e1a168a2ae709eb36
Hannah Jeror <jerorhn@yahoo.com>
2018-03-20 15:56:31 -0400

    Add topics

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,23 @@
 require 'random_data'
 
+#Create Topics
+15.times do
+  Topic.create!(
+    name: RandomData.random_sentence,
+    description: RandomData.random_paragraph
+  )
+end
+topics = Topic.all
+
 #Create Posts
 50.times do
   Post.create!(
+    topic: topics.sample,
     title: RandomData.random_sentence,
     body: RandomData.random_paragraph
   )
 end
-Post.find_or_create_by!(title: 'Sample', body: 'This is the sample.')
+
 posts = Post.all
 
 #Create Comments
@@ -17,8 +27,8 @@ posts = Post.all
     body: RandomData.random_paragraph
   )
 end
-Comment.find_or_create_by!(post: posts.sample, body: "Sample Comment")
 
 puts "Seed finished"
+puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
